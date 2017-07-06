5d3bb5507800c5549bd4abcc5e7d48b157d2a099
Michael Teti <mteti@optimalcapacity.com>
2017-07-06 14:05:04 -0400

    ComicCollector - Lesson 7 - ImagePickerController

--- ComicCollector/ComicViewController.swift
+++ ComicCollector/ComicViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ComicViewController: UIViewController {
+class ComicViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    //Outlets
     @IBOutlet weak var comicImageView: UIImageView!
-    
     @IBOutlet weak var titleTextField: UITextField!
-
+    
+    //Global Variables
+    var imagePicker = UIImagePickerController()
+    
+    //Firs
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
     @IBAction func cameraTapped(_ sender: Any) {
     }
