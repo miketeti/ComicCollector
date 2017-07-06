a14cf76a9b97bd262582b3f47a3170e61eb56e61
Michael Teti <mteti@optimalcapacity.com>
2017-07-06 13:34:06 -0400

    ComicCollector - Lesson 4 - ImageViews

---
+++ ComicCollector/ComicViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ComicViewController.swift
+//  ComicCollector
+//
+//  Created by Michael Teti on 7/6/17.
+//  Copyright © 2017 Michael Teti. All rights reserved.
+//
+
+import UIKit
+
+class ComicViewController: UIViewController {
+    @IBOutlet weak var comicImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
